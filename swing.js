59adf3ed01c39a99bc15da49b694d32ca5611eef
Craig <craigm1357@gmail.com>
2015-02-15 21:37:52 +0000

    license

--- swing.js
+++ swing.js
@@ -2,7 +2,12 @@
  * @version 1.0.9
  * @link https://github.com/gajus/swing for the canonical source repository
  * @license https://github.com/gajus/swing/blob/master/LICENSE BSD 3-Clause
+ * Copyright (c) 2014, Gajus Kuizinas (http://gajus.com/)
+ * All rights reserved.
  */
+
+
+
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 // shim for using process in browser
 
